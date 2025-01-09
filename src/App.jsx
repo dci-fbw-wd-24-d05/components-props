9b08edb27763173f9f1adcaa9632036e34509264
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-09 13:45:00 +0100

    finished array rendering with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import ImageContainer from "./components/ImageContainer";
+import beaches from "./data/beachs.json";
 
 function App() {
   return (
     <>
       <h1>Create New component</h1>
-      <ImageContainer />
-
-      <ImageContainer />
+      {beaches.map((beach) => {
+        return (
+          <ImageContainer
+            key={crypto.randomUUID()}
+            title={beach.title}
+            imageLink={beach.image}
+            discription={beach.description}
+          />
+        );
+      })}
     </>
   );
 }
