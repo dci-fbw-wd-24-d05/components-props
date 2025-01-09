1e11b216ee3b47dccfcd745b58630171838c3e1d
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-09 12:14:23 +0100

    props and distructuring live coding

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ImageDescription from "./ImageDescription";
 
-function ImageContainer() {
+function ImageContainer({ title, imageLink, discription }) {
+  // const {title, imageLink, discription} = props;
   return (
     <div>
-      <h3>Bild from unsplash</h3>
-      <img
-        src="https://images.unsplash.com/photo-1519046904884-53103b34b206?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYWNofGVufDB8fDB8fHww"
-        alt="beach image"
-      />
-      <ImageDescription />
+      <h3>{title}</h3>
+      <img src={imageLink} alt="beach image" />
+      <ImageDescription discription={discription} />
     </div>
   );
 }
--- src/components/ImageDescription.jsx
+++ src/components/ImageDescription.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-function ImageDescription() {
+function ImageDescription(props) {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore aperiam
-        ullam sit accusantium dolor fugit placeat tenetur obcaecati incidunt
-        fuga, dignissimos corporis veritatis provident magnam, quibusdam
-        molestias dolore architecto aut?
-      </p>
+      <p>{props.discription}</p>
     </div>
   );
 }
