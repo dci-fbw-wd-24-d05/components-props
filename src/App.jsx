d709d4552839edbed0ad53e4271882a6b8c20ead
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-09 11:28:18 +0100

    form container component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import FormContainer from "./components/FormContainer";
 import ImageContainer from "./components/ImageContainer";
 
 function App() {
   return (
     <>
       <h1>Create New component</h1>
+      <FormContainer />
       <ImageContainer />
       <ImageContainer />
     </>
