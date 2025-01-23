97c25ad95fc35644a45079e5461adf615dbfb010
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-23 13:31:54 +0100

    refactoring the code

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -1,15 +1,15 @@
-import ImageDescription from "./ImageDescription";
+// import ImageDescription from "./ImageDescription";
 
-function ImageContainer({ title, imageLink, discription }) {
-  // const {title, imageLink, discription} = props;
-  return (
-    <div>
-      <h3>{title}</h3>
-      <img src={imageLink} alt="beach image" />
-      <ImageDescription discription={discription} />
-      {/* <p>{discription}</p> */}
-    </div>
-  );
-}
+// function ImageContainer({ title, imageLink, discription }) {
+//   // const {title, imageLink, discription} = props;
+//   return (
+//     <div>
+//       <h3>{title}</h3>
+//       <img src={imageLink} alt="beach image" />
+//       <ImageDescription discription={discription} />
+//       {/* <p>{discription}</p> */}
+//     </div>
+//   );
+// }
 
-export default ImageContainer;
+// export default ImageContainer;
